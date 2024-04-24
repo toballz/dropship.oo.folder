efe8b84203c2a2dbdc673d55b97a43b006bc6b68
toballz <toballz@yahoo.com>
2024-04-24 02:41:20 -0400

    4/24/2024
    4/24/2024

--- .null.php
+++
@@ -1,8 +0,0 @@
-<?php include_once("./conf.php"); 
-                       
-//echo(mail("toballz@yahoo.com","dhgfjhgkjhfd dasfd","ytrgthyjkujhdsfdg"));
- 
- //    $_SESSION[session::userArrayNameKey]['id']=5462;
-?>
-
-<pre><?php print_r($_SESSION);?></pre>
