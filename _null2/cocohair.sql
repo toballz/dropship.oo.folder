eb1adb011b1220634bbb5f36d593637487da6ecb
toballz <toballz@yahoo.com>
2024-06-13 19:33:59 -0400

    coco 6/13/2024
    coco 6/13/2024

--- _null2/cocohair.sql
+++ _null2/cocohair.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 12, 2024 at 02:17 AM
+-- Generation Time: Jun 13, 2024 at 11:32 PM
 -- Server version: 10.11.7-MariaDB-cll-lve
 -- PHP Version: 7.2.34
 
@@ -31,18 +31,21 @@ CREATE TABLE `availability` (
   `id` int(11) NOT NULL,
   `namer` varchar(100) NOT NULL,
   `description` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}',
-  `description2` varchar(152) DEFAULT NULL
+  `accountEmail` varchar(30) DEFAULT NULL,
+  `accountPhone` int(13) DEFAULT NULL,
+  `accountPassword` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `availability`
 --
 
-INSERT INTO `availability` (`id`, `namer`, `description`, `description2`) VALUES
-(1, 'weekly', '{\"sunday\":\"\",\"monday\":\"0830, 1130\",\"tuesday\":\"0830, 1130\",\"wednesday\":\"0830, 1130\",\"thursday\":\"0830, 1130\",\"friday\":\"0830, 1130\",\"saturday\":\"0830, 1130\"}', NULL),
-(2, 'account', '{\n\"fullname\":\"\",\n\"email\":\"\",\n\"phonenumber\":\"\",\n\"fullname\":\"\",\n}', 'b0baee9d279d34fa1dfd71aadb908c3f'),
-(3, 'override', '[{\"date\":\"20240615\",\"time\":\"1330\"},{\"date\":\"20240612\",\"time\":\"0830\"},{\"date\":\"20240614\",\"time\":\"0830\"},{\"date\":\"20240619\",\"time\":\"1130\"},{\"date\":\"20240622\",\"time\":\"0\"},{\"date\":\"20240628\",\"time\":\"1130\"},{\"date\":\"20240611\",\"time\":\"0\"},{\"date\":\"20240620\",\"time\":\"1130\"}]', NULL),
-(4, 'message_notification', '{\"subject\":\"k qteryehgj dsfs f sf sf sfffffffff fs sd djfhjf\",\"message\":\"hgfhttps://fmoviesz.to/tv/rick-and-morty-x1zoz/3-5https:// fmoviesz.to/tvad.to/tv/zr k-and-mort -x1zoz/3-5https://fmoviesz.to/ 3-5https://fmoviesz.to/tv/rick-and-morty-x1zoz/3-5https://fmoviesz.tzmorty-x1zoz/3-5hdgffvaisreada1\"}', NULL);
+INSERT INTO `availability` (`id`, `namer`, `description`, `accountEmail`, `accountPhone`, `accountPassword`) VALUES
+(1, 'weekly', '{\"sunday\":\"\",\"monday\":\"0830, 1130\",\"tuesday\":\"0830, 1130\",\"wednesday\":\"0830, 1130\",\"thursday\":\"0830, 1130\",\"friday\":\"0830, 1130\",\"saturday\":\"0830, 1130\"}', NULL, NULL, NULL),
+(3, 'override', '[{\"date\":\"20240615\",\"time\":\"1330\"},{\"date\":\"20240614\",\"time\":\"0830\"},{\"date\":\"20240619\",\"time\":\"1130\"},{\"date\":\"20240628\",\"time\":\"1130\"},{\"date\":\"20240620\",\"time\":\"1130\"},{\"date\":\"20240622\",\"time\":\"\"}]', NULL, NULL, NULL),
+(4, 'message_notification', '{\"subject\":\"k qteryehgj dsfs f sf sf sfffffffff fs sd djfhjf\",\"message\":\"hgfhttps://fmoviesz.to/tv/rick-and-morty-x1zoz/3-5https:// fmoviesz.to/tvad.to/tv/zr k-and-mort -x1zoz/3-5https://fmoviesz.to/ 3-5https://fmoviesz.to/tv/rick-and-morty-x1zoz/3-5https://fmoviesz.tzmorty-x1zoz/3-5hdgffvaisreada1\"}', NULL, NULL, NULL),
+(5, 'hasSubscribeMonthly', 'false', NULL, NULL, NULL),
+(6, 'account', '--user', 'cocohairsignature@gmail.com', 8506, 'b59c67bf196a4758191e42f76670ceba');
 
 -- --------------------------------------------------------
 
@@ -63,6 +66,14 @@ CREATE TABLE `schedulee` (
   `hairstyle` varchar(300) NOT NULL,
   `haspaid` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `schedulee`
+--
+
+INSERT INTO `schedulee` (`rida`, `email`, `phonne`, `date`, `time`, `customername`, `image`, `price`, `timeRange`, `hairstyle`, `haspaid`) VALUES
+('000001100', 'test@example.com', '1234567890', '20330624', '1224', 'testing process', '42', '-300', '-3 hrs', 'name of this hair', 162),
+('3ged51vki', 'deondraoliver0411@gmail.com', '2627526598', '20240620', '1130', 'Deondra oliver', '27', '- $250 mid back', '3 hours', 'goddess locs (Medium) - Bring 7 bags of Jamaican twist hair, 3 bags of fretress deep twist crotchet hair and 3 nail glue. Bring 8 packs of water wave crotchet hair.', 1);
 
 --
 -- Indexes for dumped tables
@@ -88,7 +99,7 @@ ALTER TABLE `schedulee`
 -- AUTO_INCREMENT for table `availability`
 --
 ALTER TABLE `availability`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
